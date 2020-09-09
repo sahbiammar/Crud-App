eee860eb7917eaae5c6a830a3bdf3c786a8ecc3a
sahbiammar <sahbi.ammar.1995@gmail.com>
2020-09-09 21:03:15 +0200

    'update'

---
+++ src/Components/CourseList.js
@@ -0,0 +1,58 @@
+import React, { Component, Fragment } from 'react';  
+
+
+class CourseList extends Component {
+
+    state = {
+        isEdit : false
+    }
+
+    //Render Course item
+    renderCourse = () => {
+        return (
+            <li>
+                <span>{this.props.details.name}</span>
+                <button onClick={() => {this.toggleState()}}>Edit Course</button>
+                <button onClick={() => {this.props.deleteCourse(this.props.index)}}>Delete Course</button>
+            </li>
+        )
+    }
+
+    // updateCourseItem
+    updateCourseItem = (e) => {
+        e.preventDefault();
+        this.props.editCourse(this.props.index , this.input.value);
+        this.toggleState();
+    }
+
+ 
+    // ToggleState
+    toggleState = () => {             
+        let {isEdit} = this.state;
+        this.setState({
+            isEdit: !isEdit
+        })
+    }
+
+    // Render Update Form
+    renderUpdateForm = () => {
+        return (
+            <form onSubmit={this.updateCourseItem}>
+                <input type="text" ref={(v) => {this.input = v}} defaultValue={this.props.details.name}/>
+                <button>Update Course</button>   
+            </form>
+        )
+    }
+    
+
+    render() {
+        let {isEdit} = this.state;
+        return (
+            <Fragment>
+                { isEdit ? this.renderUpdateForm() : this.renderCourse() }
+            </Fragment>
+        );
+    } 
+}
+
+export default CourseList;
